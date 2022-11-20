746d0f8b2b6da5c45b5938afa7801e67312a6891
ArturTertuliano <114525297+ArturTertuliano@users.noreply.github.com>
2022-11-19 21:26:54 -0400

    Update main.py
    Signed-off-by: ArturTertuliano <114525297+ArturTertuliano@users.noreply.github.com>

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ def show_login_page():
   
         if st.session_state['loggedIn'] == False:
         
-            userName = st.text_input ("User")
+            userName = st.text_input ("Usuário")
             password = st.text_input ("Senha", type="password")
             st.button ("Entrar", on_click=LoggedIn_Clicked, args= (userName, password))
             
