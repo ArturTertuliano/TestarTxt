5cfbc6a9093023914659d26dbc91e73c6507be16
ArturTertuliano <114525297+ArturTertuliano@users.noreply.github.com>
2022-11-24 21:03:10 -0400

    Update main.py
    Signed-off-by: ArturTertuliano <114525297+ArturTertuliano@users.noreply.github.com>

--- main.py
+++ main.py
@@ -204,7 +204,8 @@ def show_login_page():
             with st.form("my_form3"):
                 userName = st.text_input ("Usuário")
                 password = st.text_input ("Senha", type="password")
-                st.form_submit_button ("Entrar", on_click=LoggedIn_Clicked, args= (userName, password))
+                st.button ("Entrar", on_click=LoggedIn_Clicked, args= (userName, password))
+                
                 
                 
 
